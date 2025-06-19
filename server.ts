af55f6e3a20e81980f59a2f7ccaae83e2e7511de
your_username <vikashraval995@gmail.com>
2025-06-19 14:26:38 +0530

    Add all project files

---
+++ server.ts
@@ -0,0 +1,44 @@
+import express from 'express';
+import { createServer } from 'http';
+import { Server } from 'socket.io';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import Message from './models/message';
+
+dotenv.config();
+
+const app = express();
+const httpServer = createServer(app);
+const io = new Server(httpServer, {
+  cors: {
+    origin: '*', // allow all origins (Android app)
+    methods: ['GET', 'POST']
+  }
+});
+
+mongoose.connect('mongodb+srv://vikashraval995:q5EKvXW7AoExUQcG@cluster0.jdj9wvu.mongodb.net/messageDB?retryWrites=true&w=majority')
+  .then(() => console.log('✅ MongoDB Connected'))
+  .catch(err => console.error('❌ MongoDB Error:', err));
+
+app.use(cors());
+app.use(express.json());
+
+// Optional REST endpoint for old messages
+app.get('/messages', async (req, res) => {
+  const messages = await Message.find();
+  res.json(messages);
+});
+
+// Socket.io communication
+io.on('connection', socket => {
+  console.log('📱 Android client connected');
+
+  socket.on('sendMessage', async data => {
+    const message = new Message({ text: data.text });
+    await message.save();
+    io.emit('receiveMessage', message); // send to all clients
+  });
+});
+
+
